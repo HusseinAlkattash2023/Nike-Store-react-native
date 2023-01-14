4e7d604501df3af8b93fe246c4634e87663470f8
hussein <asd>
2023-01-14 13:04:10 +0200

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Image , TouchableOpacity} from 'react-native';
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer , DefaultTheme} from "@react-navigation/native";
+import {icons , COLORS , FONTS , images , SIZES} from './constants'
 
-export default function App() {
+import {Home} from './screens';
+
+const theme = {
+  colors:{
+    ...DefaultTheme.colors ,
+  border:"transparent"
+  }
+}
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={theme} >
+        <Stack.Navigator
+        initialRouteName={"Home"}
+        >
+          <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title:"SHOW SELECTOR",
+            headerTintColor:COLORS.lightGray,
+            headerTitleStyle:{
+              ...FONTS.navTitle
+            },
+            headerLeft:({onPress}) => (
+              <TouchableOpacity
+              style={{marginLeft:SIZES.padding}}
+              onPress = {onPress}
+              >
+                <Image
+                source={icons.menu}
+                resizeMode="contain"
+                style={{
+                  width:25,
+                  height:25
+                }}
+                />
+              </TouchableOpacity>
+            ),
+            headerRight : () => (
+              <TouchableOpacity
+              style={{marginRight:SIZES.padding}}
+              onPress={()=> console.log("search")}
+              >
+                <Image
+                source={icons.search}
+                resizeMode="contain"
+                style={{
+                  width:30,
+                  height:30
+                }}
+                />
+              </TouchableOpacity>
+            )
+          }}
+
+          />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
+}
+
+export default ()=>{
+  return(
+    <App/>
+  )
 }
 
 const styles = StyleSheet.create({
